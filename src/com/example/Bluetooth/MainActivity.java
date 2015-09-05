b20f5cee4918e6fa5a758f685eda340f9cac9348
anOrange <75005363@qq.com>
2015-09-05 12:56:05 +0800

    chongtu

--- src/com/example/Bluetooth/MainActivity.java
+++ src/com/example/Bluetooth/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.Bluetooth;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -11,5 +12,30 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            //表明此手机不支持蓝牙
+            return;
+        }
+        if(!mBluetoothAdapter.isEnabled()){ //蓝牙未开启，则开启蓝牙
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(requestCode == RESULT_OK){
+                //蓝牙已经开启
+            }
+        }
+    }
+
 }
+
+
+//......
+
+
